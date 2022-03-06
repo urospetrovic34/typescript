c2a0c83658fa5562dc2cdab12eeb25cdb6361743
UROS <nonamen212@gmail.com>
2022-03-06 10:22:34 +0100

    Part 1: Compiling Typescript Finished

---
+++ main.js
@@ -0,0 +1,7 @@
+var character = 'waluigi';
+console.log(character);
+var input = document.querySelectorAll('input');
+console.log(input);
+input.forEach(function (i) {
+    console.log(i);
+});
---
+++ main.ts
@@ -0,0 +1,11 @@
+const character = 'waluigi'
+
+console.log(character)
+
+const input = document.querySelectorAll('input')
+
+console.log(input)
+
+input.forEach(i => {
+    console.log(i)
+})
