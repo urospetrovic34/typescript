c2a5605697d16bd949d0b62e3b8f2b9445b01476
UROS <nonamen212@gmail.com>
2022-03-06 13:21:17 +0100

    Part 3: Arrays & Objects Finished

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-var character = "mario";
-var age = 30;
-var isBlackBelt = false;
-//character = 20
-character = "luigi";
-//age = 'yoshi'
-age = 40;
-// isBlackBelt = 'yes';
-isBlackBelt = true;
-var circ = function (diameter) {
-    return diameter * Math.PI;
+// array
+var names = ["luigi", "mario", "yoshi"];
+names.push("toad");
+// names.push(3);
+// names.[0] = 3
+var numbers = [10, 20, 30, 40];
+// numbers.push(25)
+// numbers.push('shawn')
+// numbers[1] = 'shawn'
+var mixed = ["joe", 4, "mama", 8];
+mixed.push("ryu");
+mixed.push(12);
+mixed[0] = 34;
+var empty = [];
+empty.push(0);
+empty.push("34");
+empty.push(true);
+//object
+var ninja = {
+    name: "mario",
+    belt: "black",
+    age: 30
 };
-console.log(circ(7.5));
+ninja.age = 40;
+ninja.name = "ryu";
+// ninja.age = '30'
+// ninja.skills = ['fighting','sneaking']
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 40
+};
--- main.ts
+++ main.ts
@@ -1,18 +1,43 @@
-let character = "mario";
-let age = 30;
-let isBlackBelt = false;
+// array
+let names = ["luigi", "mario", "yoshi"];
 
-//character = 20
-character = "luigi";
+names.push("toad");
 
-//age = 'yoshi'
-age = 40;
+// names.push(3);
+// names.[0] = 3
 
-// isBlackBelt = 'yes';
-isBlackBelt = true;
+let numbers = [10, 20, 30, 40];
 
-const circ = (diameter:number) => {
-    return diameter * Math.PI;
+// numbers.push(25)
+// numbers.push('shawn')
+// numbers[1] = 'shawn'
+
+let mixed = ["joe", 4, "mama", 8];
+
+mixed.push("ryu");
+mixed.push(12);
+mixed[0] = 34;
+
+let empty = [];
+
+empty.push(0);
+empty.push("34");
+empty.push(true);
+
+//object
+let ninja = {
+    name: "mario",
+    belt: "black",
+    age: 30,
 };
 
-console.log(circ(7.5))
+ninja.age = 40;
+ninja.name = "ryu";
+// ninja.age = '30'
+// ninja.skills = ['fighting','sneaking']
+
+ninja = {
+    name:'yoshi',
+    belt:'orange',
+    age:40
+}
