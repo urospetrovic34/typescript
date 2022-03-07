f2d2ef8c6aa9d8f1fe9d325839c3db58ff22fa0e
UROS <nonamen212@gmail.com>
2022-03-07 12:47:06 +0100

    Part 14: Interfaces Finished

--- public/main.js
+++ public/main.js
@@ -1,3 +1,25 @@
+const me = {
+    name: "Shawn",
+    age: 30,
+    speak(text) {
+        console.log(text);
+    },
+    //we don't have to explicitly specify 
+    //this : number 'cause ts/js already
+    //knows it will return number type
+    //same goes for all other functions
+    spend(amount) {
+        console.log("I spent", amount);
+        return amount;
+    },
+};
+//later on it will have to follow the structure rules
+let someone;
+const greetPerson = (person) => {
+    console.log('hello', person.name);
+};
+greetPerson(me);
+console.log(me);
 //it is not .ts extension, rather it is .js extension
 //for importing modules we have to set composite option
 //in tsconfig to false
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,42 @@
+//interfaces
+//we don't use this to declare IsPerson object
+//we just use it to declare that if we have a variable with
+//this name in future code then it has to follow given rules
+//of the structure
+interface IsPerson {
+    name: string;
+    age: number;
+    speak(a: string): void;
+    spend(a: number): number;
+}
+
+const me: IsPerson = {
+    name: "Shawn",
+    age: 30,
+    speak(text: string): void {
+        console.log(text);
+    },
+    //we don't have to explicitly specify 
+    //this : number 'cause ts/js already
+    //knows it will return number type
+    //same goes for all other functions
+    spend(amount: number): number {
+        console.log("I spent", amount);
+        return amount;
+    },
+};
+
+//later on it will have to follow the structure rules
+let someone: IsPerson;
+
+const greetPerson = (person:IsPerson) => {
+    console.log('hello',person.name)
+}
+
+greetPerson(me)
+
+console.log(me);
+
 //it is not .ts extension, rather it is .js extension
 //for importing modules we have to set composite option
 //in tsconfig to false
