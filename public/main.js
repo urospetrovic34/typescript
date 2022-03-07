faf65bddc9bc6bbaaed52c2a169fd0598fc9e801
UROS <nonamen212@gmail.com>
2022-03-07 10:13:38 +0100

    Part 10: DOM Interaction & Typecasting Finished

--- public/main.js
+++ public/main.js
@@ -1,22 +1,23 @@
 "use strict";
-// let greet: Function;
-//example 1
-let greet;
-greet = (name, greeting) => {
-    console.log(`${name} says ${greeting}`);
-};
-//example 2
-let calc;
-calc = (numOne, numTwo, action) => {
-    if (action === "add") {
-        return numOne + numTwo;
-    }
-    else {
-        return numOne - numTwo;
-    }
-};
-//example 3
-let logDetails;
-logDetails = (ninja) => {
-    console.log(`${ninja.name} is ${ninja.age} years old`);
-};
+//exclamation mark goes at the end when we are certain
+//that the element exists in html page so it won't return
+//null possibility error/warning
+// const anchor = document.querySelector("a")!;
+// if(anchor){
+//     console.log(anchor.href);
+// }
+// console.log(anchor.href);
+// const form = document.querySelector("form")!;
+//since we are passing a class ts doesn't recognize the element
+//is a form, for this sort we use typecasting similar to C#
+const form = document.querySelector(".new-item-form");
+// console.log(form.children);
+//inputs
+const type = document.querySelector("#type");
+const toFrom = document.querySelector("#toFrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,32 @@
-// let greet: Function;
+//exclamation mark goes at the end when we are certain
+//that the element exists in html page so it won't return
+//null possibility error/warning
+// const anchor = document.querySelector("a")!;
 
-//example 1
-let greet: (a: string, b: string) => void;
+// if(anchor){
+//     console.log(anchor.href);
+// }
 
-greet = (name: string, greeting: string) => {
-    console.log(`${name} says ${greeting}`);
-};
+// console.log(anchor.href);
+// const form = document.querySelector("form")!;
+//since we are passing a class ts doesn't recognize the element
+//is a form, for this sort we use typecasting similar to C#
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+// console.log(form.children);
 
-//example 2
-let calc: (a: number, b: number, c: string) => number;
+//inputs
+const type = document.querySelector("#type") as HTMLSelectElement;
+const toFrom = document.querySelector("#toFrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
 
-calc = (numOne: number, numTwo: number, action: string) => {
-    if (action === "add") {
-        return numOne + numTwo;
-    } else {
-        return numOne - numTwo;
-    }
-};
+form.addEventListener('submit',(e:Event) => {
+    e.preventDefault()
 
-//example 3
-let logDetails: (obj: { name: string; age: number }) => void;
-
-type person = { name: string; age: number };
-
-logDetails = (ninja: person) => {
-    console.log(`${ninja.name} is ${ninja.age} years old`);
-};
+    console.log(
+        type.value,
+        toFrom.value,
+        details.value,
+        amount.valueAsNumber
+    )
+})
