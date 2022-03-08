d021c1a9647a518f8c2237ece7e3fd71bf9576a6
UROS <nonamen212@gmail.com>
2022-03-08 16:01:19 +0100

    Part 17: Generics Finished

--- public/main.js
+++ public/main.js
@@ -37,3 +37,23 @@ form.addEventListener("submit", (e) => {
     list.render(doc, type.value, "end");
     console.log(doc);
 });
+//generics
+//<T> captures whatever item we pass in on the function
+//and it also captures it's properties
+//whatever is passed in, this <T> must extend object
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUID({ name: "yoshi", age: 40 });
+console.log(docOne);
+const docThree = {
+    uid: 1,
+    resourceName: 'person',
+    data: 'shawn'
+};
+const docFour = {
+    uid: 23,
+    resourceName: 'shoppingList',
+    data: ['apple']
+};
--- src/main.ts
+++ src/main.ts
@@ -55,3 +55,36 @@ form.addEventListener("submit", (e: Event) => {
 
     console.log(doc);
 });
+
+//generics
+//<T> captures whatever item we pass in on the function
+//and it also captures it's properties
+//whatever is passed in, this <T> must extend object
+const addUID = <T extends { name: string }>(obj: T) => {
+    let uid = Math.floor(Math.random() * 100);
+    return { ...obj, uid };
+};
+
+let docOne = addUID({ name: "yoshi", age: 40 });
+
+console.log(docOne);
+
+//generics with interfaces
+//data will be of type T, which means it can be any type
+interface Resource<T> {
+    uid: number;
+    resourceName: string;
+    data: T;
+}
+
+const docThree: Resource<string> = {
+    uid:1,
+    resourceName:'person',
+    data:'shawn'
+}
+
+const docFour: Resource<string[]> = {
+    uid:23,
+    resourceName:'shoppingList',
+    data:['apple']
+}
